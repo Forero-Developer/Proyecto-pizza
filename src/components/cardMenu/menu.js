50627def7854c4db60bb60808435860bf678610b
Forero <forero.developer01@gmail.com>
2024-10-04 17:06:02 -0500

    Listo componente card

--- src/components/cardMenu/menu.js
+++ src/components/cardMenu/menu.js
@@ -8,9 +8,9 @@ const menu = [
     },
     {
         id: 2,
-        img: "./src/assets/pizzaaa.jpg",
+        img: "./src/assets/pizza2.jpg",
         imgText: "pizza 2",
-        txtcomida: "Pizza Familiar 2",
+        txtcomida: "Pizza Familiar Combo supremo 2x1",
         precio: "$50.000"
     }
 ]
