cfe1c37abc8dd74c96bcd76ead577a9154c5a83b
Forero <forero.developer01@gmail.com>
2024-10-15 20:18:20 -0500

    card organizadas

--- src/components/cardMenu/CardMenu.jsx
+++ src/components/cardMenu/CardMenu.jsx
@@ -2,14 +2,15 @@ import "./cardMenu.css"
 
 const CardMenu = ({img, imgText, txtComida, precio}) => {
   return (
-    <div className="container-target">
+    <div className="contenedor-padre">
+      <div className="container-target">
         <img src= {img} alt={imgText}/>
         <div className="nombre-precio">
             <h3>{txtComida}</h3>
             <p>{precio}</p>
         </div>
             <button className="bton-carrito">Agregar al carrito</button>
-      
+      </div>
     </div>
   )
 }
