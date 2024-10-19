dc360f776609c67d8092588812475647cfb3490c
Forero <forero.developer01@gmail.com>
2024-10-19 11:32:20 -0500

    Agregue el frontedn y Backend en un solo proyecto

---
+++ frontend-pizza/src/components/cardMenu/CardMenu.jsx
@@ -0,0 +1,19 @@
+import "./cardMenu.css"
+
+const CardMenu = ({ img = "./src/assets/pizzaaa.jpg", imgText, txtComida, descripcion, precio}) => {
+  return (
+    <div className="contenedor-padre">
+      <div className="container-target">
+        <img src= {img} alt={imgText}/>
+        <div className="nombre-precio">
+            <h3>{txtComida}</h3>
+            <p className="descripcion">{descripcion}</p>
+            <p>{precio}</p>
+        </div>
+            <button className="bton-carrito">Agregar al carrito</button>
+      </div>
+    </div>
+  )
+}
+
+export default CardMenu
