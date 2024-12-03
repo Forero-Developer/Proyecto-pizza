a6b61eeb6b2a48beaddf3f349a112dc87b2618d1
Nelson Holguin <nelson.holguin@correounivalle.edu.co>
2024-12-02 21:23:19 -0500

    Base de datos en la Nube

--- backend-pizza/server.js
+++ backend-pizza/server.js
@@ -1,35 +1,41 @@
 const express = require('express');
-const mysql = require('mysql2')
-const cors = require('cors')
+const mysql = require('mysql2');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 
+// Permite solicitudes desde el frontend (ajusta según tu configuración)
 app.use(cors({
-    origin: 'http://localhost:5174', // Origen permitido (puedes cambiar este valor según tu configuración)
+    origin: 'http://localhost:5173', // Aquí pon la URL de tu frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-  }));
-  
+}));
 
+// Configuración de conexión con la base de datos en Railway
 const DB = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'restaurante_italiano',
+    host: 'junction.proxy.rlwy.net',  // Cambia a este host proporcionado por Railway
+    user: 'root',                     // Usuario proporcionado por Railway
+
+    
+    password: 'vevzGCjrRZoaVWFCcDxKQPIXJOClvWzl',  // Contraseña proporcionada por Railway
+    database: 'pizzeria',              // Nombre de la base de datos en Railway
+    port: 11087,                      // Puerto proporcionado por Railway
+    connectTimeout: 10000             // Tiempo de espera mayor si es necesario
 });
 
-DB.connect((err) =>{
-    if (err){
+
+DB.connect((err) => {
+    if (err) {
         throw err;
     }
-    console.log('conexion exitosa!!')
+    console.log('Conexión exitosa con la base de datos en la nube');
 });
 
-//DECLARAMOS RUTA PARA OBTENER LOS PRODUCTOS DE LA BD
-//Y DEVOLVERLOS AL FRONTED EN FORAMTO JSON
+// Declaramos ruta para obtener los productos de la base de datos
 app.get('/api/productos', (req, res) => {
     const SQL_QUERY = `
-    SELECT p.nombre AS nombre, 
+    SELECT p.id_producto,  
+           p.nombre AS nombre, 
            p.descripcion, 
            pr.precio, 
            pr.fecha_inicio
@@ -54,8 +60,6 @@ app.get('/api/productos', (req, res) => {
         }
     });
 });
-
-//ECHAMOS 
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
